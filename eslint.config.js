bfc9afff522fcff0fe0f235516ab1043283c22d3
Dardo Altobelli <altobellidardo@gmail.com>
2025-07-11 22:48:39 -0300

    add ESLint configuration file for JavaScript and TypeScript

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import neostandard from 'neostandard'
+
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], languageOptions: { globals: globals.node } },
+  tseslint.configs.recommended,
+  ...neostandard({
+    ts: true
+  }),
+  globalIgnores(['dist', 'node_modules'])
+])
