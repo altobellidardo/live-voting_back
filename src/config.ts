f68b7c5ab927692a99d6a8aaa3064017e89a538b
Dardo Altobelli <altobellidardo@gmail.com>
2025-07-15 02:01:31 -0300

    add cookie-parser jwt and morgan dependencies

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const {
+  PORT = 3000,
+  SECRET,
+  NODE_ENV = 'development'
+} = process.env
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import { Server } from 'socket.io'
 import http from 'http'
-
-const PORT = process.env.PORT ?? 3000
+import { socketManager } from './services/socket'
+import { formatDate } from './utils/date'
+import roomRouter from './router/room'
+import userRouter from './router/user'
+import morgan from 'morgan'
+import cookieParser from 'cookie-parser'
+import { PORT } from './config'
 
 const app = express()
 const server = http.createServer(app)
@@ -11,22 +16,23 @@ const io = new Server(server, {
   cors: { origin: '*' }
 })
 
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:3005',
+  credentials: true
+}))
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cookieParser())
 
 app.get('/', (_req, res) => {
   res.send('API is running')
 })
 
-io.on('connection', (socket) => {
-  console.log('a user connected')
-  socket.on('disconnect', () => {
-    console.log('a user disconnected')
-  })
-  socket.on('message', (msg) => {
-    io.emit('message', msg)
-  })
-})
+app.use('/rooms', roomRouter)
+app.use('/users', userRouter)
+
+socketManager(io)
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+  console.log(`Server is running on port ${PORT} at ${formatDate(new Date())}`)
 })
